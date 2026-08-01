89a92bc0d79fbcfb5ca0b2f94234cf9b72b3552c
pieupieu16 <tranhaiquan16102006@gmail.com>
2026-08-01 10:25:33 +0700

    t6->t7

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ ZALO_GROUP_ID = os.getenv("ZALO_GROUP_ID", "")
 # LỊCH GỬI TIN NHẮN
 # ============================================================
 STANDUP_HOUR = int(os.getenv("STANDUP_HOUR", "10"))
-STANDUP_MINUTE = int(os.getenv("STANDUP_MINUTE", "24"))
+STANDUP_MINUTE = int(os.getenv("STANDUP_MINUTE", "28"))
 TIMEZONE = os.getenv("TIMEZONE", "Asia/Ho_Chi_Minh")
 
 # ============================================================
